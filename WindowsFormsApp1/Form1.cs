5eafcbf48de0fd56c387378a9ceac338379d6bc8
Анастасия Мазур < >
2017-04-17 22:10:23 +0300

    Add project files.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            byte d = 0x80;
+            textBox1.Text = d.GetHashCode().ToString();
+            //OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            //openFileDialog1.InitialDirectory = "c:\\";
+            //openFileDialog1.Filter = "xls files (*.xls)|*.xls";
+            //if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            //{
+
+            //}
+        }
+    }
+}
