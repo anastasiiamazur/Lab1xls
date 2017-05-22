0e1bdacf15a4e0c4ddd5ad34df5f1fa26a912270
Анастасия Мазур <nastyamazur5@gmail.com>
2017-05-22 23:50:22 +0300

    Metadata

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,18 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(12, 83);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(505, 268);
-            this.textBox1.TabIndex = 0;
             // 
             // openFileDialog1
             // 
@@ -47,21 +39,29 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(30, 42);
+            this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(120, 32);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "Open Excel file";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 50);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(505, 300);
+            this.textBox1.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(529, 362);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -70,10 +70,9 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -8,6 +8,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using DocumentFormat.OpenXml.Spreadsheet;
+using DocumentFormat.OpenXml.Packaging;
+
 
 namespace WindowsFormsApp1
 {
@@ -20,16 +23,39 @@ namespace WindowsFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            byte d = 0x80;
-            textBox1.Text = d.GetHashCode().ToString();
-            //OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "Excel files (*.xlsx)|*.xlsx";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if (Path.GetExtension(openFileDialog1.FileName) == ".xlsx")
+                    {
+                        using (SpreadsheetDocument sourcePresentationDocument = SpreadsheetDocument.Open(openFileDialog1.FileName, true))
+                        {
+                            textBox1.Text += "Author:         " + sourcePresentationDocument.PackageProperties.Creator + Environment.NewLine;
+                            textBox1.Text += "Title:          " + sourcePresentationDocument.PackageProperties.Title + Environment.NewLine;
+                            textBox1.Text += "Subject:        " + sourcePresentationDocument.PackageProperties.Subject + Environment.NewLine;
+                            textBox1.Text += "Category:       " + sourcePresentationDocument.PackageProperties.Category + Environment.NewLine;
+                            textBox1.Text += "Keywords:       " + sourcePresentationDocument.PackageProperties.Keywords + Environment.NewLine;
+                            textBox1.Text += "Description:    " + sourcePresentationDocument.PackageProperties.Description + Environment.NewLine;
+                            textBox1.Text += "ContentStatus:  " + sourcePresentationDocument.PackageProperties.ContentStatus + Environment.NewLine;
+                            textBox1.Text += "Revision:       " + sourcePresentationDocument.PackageProperties.Revision + Environment.NewLine;
+                            textBox1.Text += "Created:        " + sourcePresentationDocument.PackageProperties.Created.ToString() + Environment.NewLine;
+                            textBox1.Text += "Modified:       " + sourcePresentationDocument.PackageProperties.Modified.ToString() + Environment.NewLine;
+                            textBox1.Text += "LastModifiedBy: " + sourcePresentationDocument.PackageProperties.LastModifiedBy + Environment.NewLine;
 
-            //openFileDialog1.InitialDirectory = "c:\\";
-            //openFileDialog1.Filter = "xls files (*.xls)|*.xls";
-            //if (openFileDialog1.ShowDialog() == DialogResult.OK)
-            //{
+                        }
+                    }
+                    else textBox1.Text = "ERROR";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
 
-            //}
+            }
         }
     }
 }
